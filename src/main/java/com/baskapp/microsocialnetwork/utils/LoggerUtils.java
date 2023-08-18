98790d48d59ed8c7925119cac34179e29e15d7e3
Yago Taveiros Ferreira <ytf@ic.ufal.br>
2023-08-18 15:56:50 -0300

    modify logs

--- src/main/java/com/baskapp/microsocialnetwork/utils/LoggerUtils.java
+++ src/main/java/com/baskapp/microsocialnetwork/utils/LoggerUtils.java
@@ -1,22 +1,46 @@
 package com.baskapp.microsocialnetwork.utils;
 
 import com.baskapp.microsocialnetwork.entities.Verbs;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.util.Map;
 
 public abstract class LoggerUtils {
     public static void requestLog(Verbs verb, String route, Map<String, String> headers) {
-        System.out.println("########### REQUEST ###########");
+        System.out.println("----------- REQUEST -----------");
         System.out.println(verb);
         System.out.println("ROUTE =:: " + route);
         System.out.println("HEADERS =:: " + headers);
-        System.out.println("###############################");
+        System.out.println("-------------------------------");
+    }
+    public static void requestLog(Verbs verb, String route, Map<String, String> headers, Object body) {
+        ObjectMapper objectMapper = new ObjectMapper();
+        String requestBody;
+        try {
+            requestBody = objectMapper.writeValueAsString(body);
+        } catch (JsonProcessingException e) {
+            requestBody = "";
+        }
+        System.out.println("----------- REQUEST -----------");
+        System.out.println(verb);
+        System.out.println("ROUTE =:: " + route);
+        System.out.println("HEADERS =:: " + headers);
+        System.out.println("BODY =:: " + requestBody);
+        System.out.println("-------------------------------");
     }
 
     public static void responseLog(String route, Object body) {
-        System.out.println("########### RESPONSE ###########");
+        ObjectMapper objectMapper = new ObjectMapper();
+        String responseBody;
+        try {
+            responseBody = objectMapper.writeValueAsString(body);
+        } catch (JsonProcessingException e) {
+            responseBody = "";
+        }
+        System.out.println("----------- RESPONSE -----------");
         System.out.println("ROUTE =:: " + route);
-        System.out.println("RESPONSE =:: " + body);
-        System.out.println("################################");
+        System.out.println("RESPONSE =:: " + responseBody);
+        System.out.println("--------------------------------");
     }
 }
